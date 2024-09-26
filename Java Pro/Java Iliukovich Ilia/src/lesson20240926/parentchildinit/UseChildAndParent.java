a973fd450ce2f05c3a59f96cd5d878093029755f
Yevhen Kariev <mediams@mail.ru>
2024-09-26 22:16:04 +0200

    26-09-2024 22:16 from PC 'Yevhen Kariev'

--- Java Pro/Java Iliukovich Ilia/src/lesson20240926/parentchildinit/UseChildAndParent.java
+++ Java Pro/Java Iliukovich Ilia/src/lesson20240926/parentchildinit/UseChildAndParent.java
@@ -10,8 +10,8 @@ public class UseChildAndParent {
 
 //        Child.staticMethod();
         Child child = new Child();
-//        child.nonStaticMethod();
-//
+        child.nonStaticMethod();
+
 //        System.out.println(Child.staticValue);
 //        System.out.println(Parent.staticValue);
     }
