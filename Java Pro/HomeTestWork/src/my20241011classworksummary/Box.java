6a4a354495aebb40e296f0ec8e0eb519c04bb251
Yevhen Kariev <mediams@mail.ru>
2024-10-11 11:28:45 +0200

    11-10-2024 11:28 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/my20241011classworksummary/Box.java
@@ -0,0 +1,21 @@
+package my20241011classworksummary;
+
+public class Box {
+    private String name;
+    private boolean canBeSold;
+    private int count;
+    private Cat cat;
+
+    //
+    public void empty(){
+
+    }
+    // поверхностностное копирование shallow copy ссылка на один обьект
+    public void makeCopy(){
+
+    }
+    //разные обьекты
+    public void deepCopy(){
+
+    }
+}
---
+++ Java Pro/HomeTestWork/src/my20241011classworksummary/Cat.java
@@ -0,0 +1,8 @@
+package my20241011classworksummary;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String colour;
+
+}
