ecad94be80f868376825a88bdbe96c7ba2a7224a
Yevhen Kariev <mediams@mail.ru>
2024-10-11 20:53:27 +0200

    11-10-2024 20:53 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241011classworksummary/Cat.java
+++
@@ -1,8 +0,0 @@
-package my20241011classworksummary;
-
-public class Cat {
-    private String name;
-    private int age;
-    private String colour;
-
-}
---
+++ Java Pro/HomeTestWork/src/my20241011classworksummary/catinbox/Cat.java
@@ -0,0 +1,57 @@
+package my20241011classworksummary.catinbox;
+
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String colour;
+
+    public Cat(String name, int age, String colour) {
+        this.name = name;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+        return age == cat.age && Objects.equals(name, cat.name) && Objects.equals(colour, cat.colour);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+}
