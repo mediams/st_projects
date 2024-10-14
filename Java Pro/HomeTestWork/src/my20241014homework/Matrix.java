7ff11138001c63eac4557b64402a436ee407a5d1
Yevhen Kariev <mediams@mail.ru>
2024-10-14 21:16:23 +0200

    14-10-2024 21:16 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241014homework/Matrix.java
+++ Java Pro/HomeTestWork/src/my20241014homework/Matrix.java
@@ -4,23 +4,26 @@ import java.util.Arrays;
 
 public class Matrix {
     public static void main(String[] args) {
-        generateMatrix();
+        generateMatrix(4, 3);
     }
 
-    public static void generateMatrix() {
-        int n = 4, m = 3;
+    public static void generateMatrix(int n, int m) {
         int[][] array = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i == j) {
+                if ((i + j) == (m - 1)) {
                     array[i][j] = 1;
                 }
-
+            }
+        }
+        System.out.println(Arrays.deepToString(array));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("-" + Arrays.deepToString(array));
     }
 
 }
