7c4043f65dd312938781f6d6decc10a1a0710215
Yevhen Kariev <mediams@mail.ru>
2024-10-14 22:46:45 +0200

    14-10-2024 22:46 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241014homework/MaxOfColumn.java
+++ Java Pro/HomeTestWork/src/my20241014homework/MaxOfColumn.java
@@ -1,9 +1,27 @@
 package my20241014homework;
 
+import java.util.Arrays;
+
 public class MaxOfColumn {
     public static void main(String[] args) {
-        int [][] arrays = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+        int[][] arrays = {{21, 2, 3}, {4, 15, 6}, {7, 8, 9}};
+        int n = arrays.length;
+        int m = arrays[0].length;
 
-        
+        int[] newArray = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int temp = arrays[0][i];
+
+            for (int j = 0; j < n; j++) {
+                if (arrays[j][i] > temp) {
+                    temp = arrays[j][i];
+                }
+            }
+            newArray[i]= temp;
+        }
+        System.out.println(Arrays.toString(newArray));
     }
 }
+
+
