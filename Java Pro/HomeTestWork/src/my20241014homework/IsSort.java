61aa184ce988834ea95de240da25cc23a52d35a3
Yevhen Kariev <mediams@mail.ru>
2024-10-14 21:28:33 +0200

    14-10-2024 21:28 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/my20241014homework/IsSort.java
@@ -0,0 +1,18 @@
+package my20241014homework;
+
+public class IsSort {
+    public static void main(String[] args) {
+        isSort(new int[]{1, 0, 4, 5});
+    }
+
+    private static void isSort(int[] ints) {
+        boolean check = true;
+        for (int i = 0; i < ints.length - 1; i++) {
+            if (ints[i] > ints[i + 1]) {
+                check = false;
+                break;
+            } 
+        }
+        System.out.println("check = " + check);
+    }
+}
