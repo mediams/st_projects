83d930b4497d27e39860f7f5f35edac1b4d81634
Yevhen Kariev <mediams@mail.ru>
2024-10-17 16:19:27 +0200

    17-10-2024 16:19 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241017class/customiterator/MyArrayList.java
+++ Java Pro/HomeTestWork/src/my20241017class/customiterator/MyArrayList.java
@@ -1,6 +1,7 @@
 package my20241017class.customiterator;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MyArrayList implements Iterable<String> {
 
@@ -14,6 +15,46 @@ public class MyArrayList implements Iterable<String> {
 
     @Override
     public Iterator<String> iterator() {
+        return new MyReverceIterator();
+    }
+
+    public Iterator<String> reverseIterator() {
         return new MyIterator();
+    }
+
+    public class MyIterator implements Iterator<String> {
+
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return data.length > index;
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return data[index++];
+        }
+    }
+
+    public class MyReverceIterator implements Iterator<String> {
+
+        private int index = data.length-1;
+
+        @Override
+        public boolean hasNext() {
+            return index >= 0;
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return data[index--];
+        }
     }
 }
