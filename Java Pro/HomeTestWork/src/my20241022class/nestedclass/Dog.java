9fe45db649eba62ac32487763d3f11ba60fda5be
Yevhen Kariev <mediams@mail.ru>
2024-10-22 16:53:30 +0200

    22-10-2024 16:53 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/my20241022class/nestedclass/Dog.java
@@ -0,0 +1,24 @@
+package my20241022class.nestedclass;
+
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public static class DogFood{
+        private String foodName;
+
+        public DogFood(String foodName) {
+            this.foodName = foodName;
+        }
+
+        @Override
+        public String toString() {
+            return "DogFood{" +
+                    "foodName='" + foodName + '\'' +
+                    '}';
+        }
+    }
+}
