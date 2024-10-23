2c631783fa092d94461c682ebe27bf3235fdd3b5
Yevhen Kariev <mediams@mail.ru>
2024-10-23 21:53:24 +0200

    23-10-2024 21:53 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241023homework/Library.java
+++ Java Pro/HomeTestWork/src/my20241023homework/Library.java
@@ -9,6 +9,18 @@ public class Library {
         private String bookName;
         private String autorName;
 
+        public Book(String bookName, String autorName) {
+            this.bookName = bookName;
+            this.autorName = autorName;
+        }
+
+        public String getBookName() {
+            return bookName;
+        }
+
+        public String getAutorName() {
+            return autorName;
+        }
     }
 
     class LibraryCatalog {
@@ -18,14 +30,32 @@ public class Library {
             bookList.add(book);
         }
 
-        public void searchBook(String query){
-            Comparator comparator = new Comparator() {
+        public void printCatalog() {
+            for (Book book : bookList) {
+                System.out.println(book.getBookName() + " " + book.getAutorName());
+            }
+        }
+
+        public void searchBook(String query) {
+            System.out.println("Wir suchen: " + query);
+            Comparator<Book> comparator = new Comparator<Book>() {
                 @Override
-                public int compare(Object o1, Object o2) {
-                    return 0;
+                public int compare(Book book1, Book book2) {
+                    System.out.println("+");
+                    return book1.getAutorName().compareTo(book2.getAutorName());
                 }
             };
 
         }
+    }
+
+    public static void main(String[] args) {
+        Library library = new Library();
+        LibraryCatalog libraryCatalog = library.new LibraryCatalog();
+        libraryCatalog.addBook(new Book("Ein Killer", "John Müller"));
+        libraryCatalog.addBook(new Book("Eine andere Geschichte", "Anna Schmidt"));
+
+        libraryCatalog.printCatalog();
+        libraryCatalog.searchBook("John Müller");
     }
 }
