b8818f3cef1196324481e79e898827661c803408
Yevhen Kariev <mediams@mail.ru>
2024-10-24 21:51:44 +0200

    24-10-2024 21:51 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241024homework/Main.java
+++ Java Pro/HomeTestWork/src/my20241024homework/Main.java
@@ -3,15 +3,61 @@ package my20241024homework;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>(List.of("Test", "Rot", "Gorg", "Ya", "Boba", "Grob"));
 
+        LinkedList<Item> items = new LinkedList<>();
+        items.add(new Item("One", "First item"));
+        items.add(new Item("Two", "Second item"));
+        items.add(new Item("Three", "Third item"));
+
+        LinkedList<Integer> listNum = new LinkedList<>(List.of(1, 4, 3, 2, 1, 2, 2, 0));
+
         searchShort(list); //Перебрать LinkedList и найти самую короткую строку.
 
         toOneString(list); //Перебрать LinkedList и объединить все строки в одну с использованием разделителя "|".
 
+        deleteItem(items, "Third item");//Создать LinkedList с объектами вашего собственного класса и удалить все элементы, удовлетворяющие определенному условию.
+
+
+        int element = 2;
+        ListIterator<Integer> listiterator = listNum.listIterator();
+        int first = -1, last = -1;
+        while (listiterator.hasNext()) {
+            int next = listiterator.next();
+//            int previous = listiterator.previous();
+
+            System.out.println("OP");
+            if (element == next && first == -1) {
+                first = listiterator.nextIndex();
+            }
+
+            //        while (listiterator.hasNext()) {
+//            int previous = listiterator.previous();
+//            if (element == previous && first == -1) {
+//                first = listiterator.nextIndex();
+//            }
+//        }
+
+        }
+        System.out.println(first);
+
+    }
+
+    private static void deleteItem(LinkedList<Item> items, String userSearch) {
+        System.out.println("До удаления: \t" + items);
+
+        Iterator<Item> iterator = items.iterator();
+        while (iterator.hasNext()) {
+            Item i = iterator.next();
+            if (i.getName().equals(userSearch) || i.getDescription().equals(userSearch)) {
+                iterator.remove();
+            }
+        }
+        System.out.println("После удаления:\t" + items);
     }
 
     private static void toOneString(LinkedList<String> list) {
