cd8e49caab7499e523ba6d38e379a8b0d665bc44
Yevhen Kariev <mediams@mail.ru>
2024-10-25 09:21:08 +0200

    25-10-2024 09:21 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/TEST/Main.java
+++ Java Pro/HomeTestWork/src/TEST/Main.java
@@ -1,9 +1,8 @@
-package my20241024homework;
+package TEST;
 
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
