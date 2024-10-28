76ddae195118d5eee0b89534b2de6b8f5d4141c9
Yevhen Kariev <mediams@mail.ru>
2024-10-28 18:44:13 +0100

    28-10-2024 18:44 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241028algorithms/RecurExample.java
+++ Java Pro/HomeTestWork/src/my20241028algorithms/RecurExample.java
@@ -2,15 +2,33 @@ package my20241028algorithms;
 
 public class RecurExample {
     public static void main(String[] args) {
-        System.out.println(sumIteration(5));
+        int n = 3;
+        System.out.println(sumIteration(n));
+        System.out.println(sumRecursion(n));
     }
 
-    private static int sumIteration(int n) {
+
+    //Interactive approach
+
+    private static int sumIteration(int n) { //O(n)
         int sum = 0;
         for (int i = 0; i <= n; i++) {
             sum += i;
             System.out.println("sum = " + sum + " n = " + n);
         }
+        return sum;
+    }
+
+    private static int sumRecursion(int n) {
+        //Base
+        if (n == 1) {
+            return 1;
+        }
+
+//        int sum = n + sumRecursion(n - 1);
+        int result = sumRecursion(n - 1);
+        int sum = n + result;
+        System.out.println("sum = " + sum + " n = " + n);
         return sum;
     }
 }
