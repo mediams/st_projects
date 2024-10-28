485c433c9436df1ad78d73a195555ed30932c8de
Yevhen Kariev <mediams@mail.ru>
2024-10-28 18:59:40 +0100

    28-10-2024 18:59 from PC 'Yevhen Kariev'

--- DataBase/first.sql
+++ DataBase/first.sql
@@ -1004,64 +1004,59 @@ FROM employees;
 DESCRIBE employees; -- Das ist sehr interesant
 
 
--- Homework 27-10-2024
+-- summary lesson;
 --
 
-DROP DATABASE goods;
-
-CREATE DATABASE goods;
-USE goods;
-CREATE TABLE goods (
-id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(30),
-quantity INT NOT NULL CHECK (quantity>0),
-instock CHAR (1) NOT NULL CHECK (instock IN ('Y', 'N'))
-);
-
-insert into goods (id, title, quantity, instock) values (1, 'Велосипед', 2, 'Y');
-insert into goods (id, title, quantity, instock) values (2, 'Скейт', 4, 'Y');
-insert into goods (id, title, quantity, instock) values (3, 'Самокат', 2, 'Y');
-insert into goods (id, title, quantity, instock) values (4, 'Сноуборд', 7, 'N');
-insert into goods (id, title, quantity, instock) values (5, 'Санки', 1, 'Y');
-insert into goods (id, title, quantity, instock) values (6, 'Коньки', 3, 'N');
-insert into goods (id, title, quantity, instock) values (7, 'Ролики', 5, 'Y');
-insert into goods (title, quantity, instock) values ('Шлем', 8, 'Y');
-insert into goods (title, quantity, instock) values ('Футбольный мяч', 2, 'N');
-insert into goods (title, quantity, instock) values ('перчатки', 5, 'Y');
-
-CREATE TABLE goods_2 (
-id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(30),
-quantity INT NOT NULL CHECK (quantity>0),
-price INT NOT NULL,
-in_stock CHAR (1) NOT NULL CHECK(in_stock IN('Y','N'))
-);
-
-insert into goods_2 (id, title, quantity, price, in_stock) values (1, 'Тюбинг', 5, 1000, 'Y');
-insert into goods_2 (id, title, quantity, price, in_stock) values (2, 'Санки', 2, 1500, 'Y');
-insert into goods_2 (id, title, quantity, price, in_stock) values (3, 'Снегокат', 2, 900, 'Y');
-insert into goods_2 (id, title, quantity, price, in_stock) values (4, 'Сноуборд', 7, 700, 'N');
-insert into goods_2 (id, title, quantity, price, in_stock) values (5, 'Клюшка', 8, 300, 'Y');
-insert into goods_2 (id, title, quantity, price, in_stock) values (6, 'Коньки', 3, 350, 'N');
-insert into goods_2 (id, title, quantity, price, in_stock) values (7, 'Форма', 9, 450, 'Y');
-insert into goods_2 ( title, quantity, price, in_stock) values ( 'Гантели', 9, 450, 'Y');
-insert into goods_2 (  title, quantity, price, in_stock) values ( 'Форма', 9, 450, 'Y');
-insert into goods_2 ( title, quantity, price, in_stock) values ( 'Боксерские перчатки', 9, 450, 'Y');
-
-SELECT title FROM goods
-UNION ALL
-SELECT title FROM goods_2;
-
-SELECT title FROM goods
-UNION
-SELECT title FROM goods_2;
+USE hr;
 
 SELECT
-t1.id, t1.title, t1.quantity, 
-t2.price, t2.in_stock AS instock
-FROM goods AS t1
-INNER JOIN goods_2 AS t2
-ON t1.title = t2.title;
+t1.*,
+t2.*
+FROM employees AS t1
+INNER JOIN employees AS t2
+ON t1.manager_id = t2.employee_id AND t1.salary>t2.salary;
 
-SELECT * FROM goods;
-SELECT * FROM goods_2;
+
+SELECT
+t1.country_name,
+t2.region_name
+FROM countries AS t1
+INNER JOIN regions AS t2
+ON t1.region_id = t2.region_id;
+
+
+SELECT
+t1.city,
+t1.state_province,
+t2.country_name
+FROM locations AS t1
+INNER JOIN countries AS t2
+ON t1.country_id = t2.country_id;
+
+SELECT
+l.city,
+l.state_province,
+c.country_name,
+r.region_name
+FROM locations AS l
+INNER JOIN countries AS c
+ON l.country_id = c.country_id
+INNER JOIN regions AS r
+ON c.region_id = r.region_id;
+
+
+SELECT
+t1.first_name,
+t1.last_name,
+t3.city
+FROM employees AS t1
+INNER JOIN departments AS t2
+ON t1.department_id = t2.department_id
+INNER JOIN locations AS t3
+ON t2.location_id = t3.location_id AND t3.city IN('Seattle','Toronto');
+
+
+SELECT * FROM locations;
+SELECT * FROM countries;
+SELECT * FROM regions;
+SELECT * FROM employees;
