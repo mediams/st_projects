1e1bd955d6dbb36b380508cb716a061ee00cd809
Yevhen Kariev <mediams@mail.ru>
2024-10-29 15:02:50 +0100

    29-10-2024 15:02 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241029practise/Book.java
+++ Java Pro/HomeTestWork/src/my20241029practise/Book.java
@@ -52,6 +52,14 @@ public class Book {
 
         }
     }
+    public static class BookComparatorByPrice implements Comparator<Book> {
+        @Override
+        public int compare(Book o1, Book o2) {
+//            return o1.getPrice().compareTo(o2.getAuthor());
+            return Double.compare(o1.getPrice(), o2.getPrice());
+
+        }
+    }
 
     @Override
     public String toString() {
--- Java Pro/HomeTestWork/src/my20241029practise/PractiseEx.java
+++ Java Pro/HomeTestWork/src/my20241029practise/PractiseEx.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 public class PractiseEx {
     public static void main(String[] args) {
-        Book book1 = new Book("Rawling", "Harry Potter und der Stein der Weisen", 10.0, true, 10);
-        Book book2 = new Book("Rawling", "Harry Potter und die Kammer des Schreckens", 10.0, true, 10);
+        Book book1 = new Book("Rawling", "Harry Potter", 10.0, true, 10);
+        Book book2 = new Book("Rawling", "Harry Potter", 10.0, true, 10);
         Book book3 = new Book("", "Java in Action", 14.0, true, 19);
         Book book4 = new Book("M. Twain", "Tom Sawyer", 6.0, true, 15);
         Book book5 = new Book("Rl: Tolstoy", "War and Peace", 7.0, true, 12);
         Book book6 = new Book("Sedgwik", "Algorithms", 21.0, true, 14);
-        Book book7 = new Book("Rawling", "Harry Potter und der Gefangene von Askaban", 10.0, true, 10);
+        Book book7 = new Book("Rawling", "Harry Potter", 10.0, true, 10);
 
         List<Book> books = new ArrayList<>(List.of(book1, book2, book3, book4, book5, book6, book7));
 
         sortCompare(books, new Book.BookComparatorByTitle());
         System.out.println(books);
 
-
-        sortCompare(books, new Book.BookComparatorByAuthor());
+        sortCompare(books, new Book.BookComparatorByPrice());
+//        sortCompare(books, new Book.BookComparatorByAuthor());
         System.out.println(books);
 
     }
@@ -33,13 +33,18 @@ public class PractiseEx {
             for (int j = 0; j < books.size() - i - 1; j++) {
                 isSorted = true;
                 if (comparator.compare(books.get(j), books.get(j + 1)) > 0) {
-                    temp = books.get(j);
-                    books.set(j, books.get(j + 1));
-                    books.set((j + 1), temp);
+                    swap(books, j, j + 1);
                     isSorted = false;
                 }
             }
             if (isSorted) break;
         }
+    }
+
+    private static void swap(List<Book> books, int j, int i) {
+        Book temp;
+        temp = books.get(j);
+        books.set(j, books.get(i));
+        books.set((i), temp);
     }
 }
