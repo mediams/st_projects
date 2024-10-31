305d1d0e9572a90cb6ca14b1b007c150599e92e5
Yevhen Kariev <mediams@mail.ru>
2024-10-31 11:12:12 +0100

    31-10-2024 11:12 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241029practise/PractiseEx.java
+++ Java Pro/HomeTestWork/src/my20241029practise/PractiseEx.java
@@ -18,6 +18,7 @@ public class PractiseEx {
         List<Book> books = new ArrayList<>(List.of(book1, book2, book3, book4, book5, book6, book7));
 
         sortCompare(books, new Book.BookComparatorByTitle());
+        Collections.sort(books, new Book.BookComparatorByPrice());
         System.out.println(books);
 
         sortCompare(books, new Book.BookComparatorByPrice());
@@ -25,20 +26,24 @@ public class PractiseEx {
         System.out.println(books);
 
     }
-
+// какие вопросы будут на собесе
     public static void sortCompare(List<Book> books, Comparator<Book> comparator) {
-        for (int i = 0; i < books.size(); i++) {
+        // first operation - make copy to array or ArrayList
+        List<Book> booksCopy = new ArrayList<>(books); // off jetzt
+        for (int i = 0; i < books.size(); i++) { // n
             boolean isSorted = true;
             Book temp = null;
-            for (int j = 0; j < books.size() - i - 1; j++) {
-                if (comparator.compare(books.get(j), books.get(j + 1)) > 0) {
-                    swap(books, j, j + 1);
+            for (int j = 0; j < books.size() - i - 1; j++) { // n/2
+                if (comparator.compare(books.get(j), books.get(j + 1)) > 0) { // ArrayList 1, LinkidList On
+                    swap(books, j, j + 1); // von 1 ---- bis n
                     isSorted = false;
                 }
             }
             if (isSorted) break;
         }
-    }
+    } // n * n / 2 * n ----> O(n^3) for LinkedLIst
+    // n * n / 2 ----> O(n^2) for ArrayList
+    // last operation - write sorted copy to books
 
     private static void swap(List<Book> books, int j, int i) {
         Book temp;
@@ -46,4 +51,28 @@ public class PractiseEx {
         books.set(j, books.get(i));
         books.set((i), temp);
     }
+
+    // besser als swap
+    public static void swap2(List<Book> books, int i, int j) {
+        if (i != j) {
+            Book temp = books.get(i);
+            books.set(i, books.get(j));
+            books.set(j, temp);
+        }
+    }
+
+    public static void selectionSort(List<Book> books, Comparator<Book> comparator2) {
+        for (int i = 0; i < books.size(); i++) {
+            Book minBook = books.get(i);
+            int indexMinBook = i;
+            for (int j = i + 1; i < books.size(); i++) {
+                if (comparator2.compare(minBook, books.get(j)) > 0) {
+                    minBook = books.get(j);
+                    indexMinBook = j;
+                    swap2(books, i, indexMinBook);
+                }
+            }
+        }
+    }
 }
+
