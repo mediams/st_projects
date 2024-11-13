550c3d2eb354d0f743d90c240da97cd1215e5505
Yevhen Kariev <mediams@mail.ru>
2024-11-13 18:37:36 +0100

    13-11-2024 18:37 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241112class/functionalinterfaces/StandartFunctionalInterfaces.java
+++ Java Pro/HomeTestWork/src/my20241112class/functionalinterfaces/StandartFunctionalInterfaces.java
@@ -1,12 +1,42 @@
 package my20241112class.functionalinterfaces;
 
-import java.util.Random;
+import java.util.*;
 import java.util.function.*;
 
 public class StandartFunctionalInterfaces {
     public static void main(String[] args) {
         Consumer<Integer> printData = integer -> System.out.println(integer);
         printData.accept(777);
+
+//        Какие из выражений вернут true для массивов ниже?
+        System.out.println("-------------------------");
+        int[][] array1 = {{1, 2}, {3, 4}};
+        int[][] array2 = {{1, 2}, {3, 4}};
+        System.out.println(Arrays.equals(array1, array2));
+        System.out.println(array1.equals(array2));
+        System.out.println(array1 == array2);
+        System.out.println(Arrays.deepEquals(array1, array2));
+        System.out.println("-------------------------");
+        System.out.println("-------------------------");
+        String[] array = new String[3];
+        String text = "";
+        for (int i = 0; i < 3; i++) {
+            text+=array[i];
+        }
+        System.out.println(text);
+
+        Set<Boolean> set1 = new TreeSet<>();
+        Set<Boolean> set2 = new HashSet<>();
+        set1.add(true); // 1
+        set2.add(true); // 2
+
+        List<Long> list = new ArrayList<>();
+        list.add(2L);
+        list.add(3L);
+        list.add(null);
+        list.sort(Comparator.nullsFirst(Comparator.naturalOrder()));
+        System.out.println(list);
+
 
         Supplier<String> generateString = () -> new Random().nextInt(10) + ", data";
         System.out.println(generateString.get());
@@ -41,6 +71,15 @@ public class StandartFunctionalInterfaces {
  * <p>
  * принимает данные и возвращает другие данные. (data -> data2)
  * Пример: Function<String, Integer> length = s -> s.length();
+ * <p>
+ * ничего не принимает, но возвращает данные. (void -> data)
+ * Пример: Supplier<String> provideText = () -> "Sample Text";
+ * <p>
+ * принимает данные и возвращает другие данные. (data -> data2)
+ * Пример: Function<String, Integer> length = s -> s.length();
+ * <p>
+ * это функциональный интерфейс, который принимает один аргумент и возвращает результат того же типа. Если упрощенно: "data1 -> data2", где data1 и data2 одного и того же типа.
+ * Его часто используют для модификации или обработки одного объекта и возвращения изменённого объекта того же типа.
  */
 //void -> data ---- Supplier
 /**
