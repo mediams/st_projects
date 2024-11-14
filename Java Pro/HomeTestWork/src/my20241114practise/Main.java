d2edbaafcf189f8bf0b99cb364f70db0f7cc74a9
Yevhen Kariev <mediams@mail.ru>
2024-11-14 14:12:35 +0100

    14-11-2024 14:12 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/my20241114practise/Main.java
@@ -0,0 +1,40 @@
+package my20241114practise;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringGenerator stringGenerator = new StringGenerator() {
+            StringBuilder newString = new StringBuilder();
+
+            @Override
+            public String method() {
+                for (char i = 'A'; i <= 'Z'; i++) {
+                    newString.append(i);
+                }
+                return newString.toString();
+            }
+        };
+        System.out.println(stringGenerator.method());
+
+        
+        new Main().generate(() -> {
+            StringBuilder newString = new StringBuilder();
+            for (char i = 'A'; i <= 'Z'; i++) {
+                newString.append(i);
+            }
+            return newString.toString();
+        });
+    }
+
+    public String generate (StringGenerator stringGenerator){
+
+        return stringGenerator.method();
+    }
+
+
+}
+//Создайте функциональный интерфейс StringGenerator,
+// который не принимает параметров и возвращает случайную строку длиной от 5 до 10 символов,
+
+// содержащую случайные буквы. Реализуйте его и сгенерируйте 10 случайных строк, используя этот интерфейс.
---
+++ Java Pro/HomeTestWork/src/my20241114practise/StringGenerator.java
@@ -0,0 +1,8 @@
+package my20241114practise;
+
+import my20241114class.lambdasanonimusclass.Task;
+
+@FunctionalInterface
+public interface StringGenerator {
+    String method();
+}
