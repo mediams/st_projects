db5d7d9c0d63c31f807e89ce6da80de57853f45f
Yevhen Kariev <mediams@mail.ru>
2024-11-18 21:08:52 +0100

    18-11-2024 21:08 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/my20241118homework/Main.java
@@ -0,0 +1,74 @@
+package my20241118homework;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        1. Создать Supplier randomJoke, который выдает пользователю случайный анекдот из заготовленного исходного списка анекдотов.
+        List<String> listJoke = List.of(
+                "Почему программистам сложно дружить с детьми? Потому что они начинают считать с нуля, а дети с года.",
+                "Программистов нельзя спрашивать о том, как дела. У них всегда три состояния: 1) всё работает; 2) не работает; 3) не могу воспроизвести баг.",
+                "Программист объясняет жене: \"Я тут код пишу.\" Она отвечает: \"А почему тогда не печатаешь?\" Программист: \"Я пишу его мышкой!\"",
+                "Программист подошел к зеркалу, задумался и говорит: \"Привет, а ты кто будешь?\" Зеркало отвечает: \"404. Эго не найдено.\"",
+                "Почему программисты любят зиму? Потому что в двоичном формате она означает «Off»!"
+        );
+
+        Supplier<String> randomJoke = () -> {
+            Random random = new Random();
+            return listJoke.get(random.nextInt(listJoke.size()));
+        };
+        System.out.println(randomJoke.get());
+
+//        2. Создать Consumer, который принимает числа и печатает четные числа в поток System.out, а нечетные числа в System.err.
+
+        Consumer<Integer> checkIsEven = (num) -> {
+            if (num % 2 == 0) {
+                System.out.println("Even: " + num);
+            } else System.err.println("Odd: " + num);
+        };
+
+        checkIsEven.accept(-1);
+
+//        3. Создать три функции:
+
+
+
+
+//        Составить композитную функцию так, чтобы:
+
+//        Input:
+//        777
+//        Output:
+//        8
+
+//        Input:
+//        "Hello!"
+//        Output:
+//        "<<60>>"
+
+        System.out.println("//        f1 - умножает Integer на 10");
+        UnaryOperator<Integer> multiply = (num) -> num * 10;
+        System.out.println(multiply.apply(777));
+
+        System.out.println("//        f2 - преобразует Integer в String в формате <<123>>");
+        Function<Integer, String> toString = (num) -> {
+            String str = "<<" + num + ">>";
+            return str;
+        };
+        System.out.println(toString.apply(5));
+
+        System.out.println("//        f3 - считает число символов в String");
+        Function<String, Integer> lengthOfSring = (str) -> str.length();
+        System.out.println(lengthOfSring.apply("Text"));
+
+        System.out.println("//        а) функции выполнялись последовательно друг за другом f1 -> f2 -> f3");
+        Function<Integer, Integer> andThen = multiply.andThen(toString.andThen(lengthOfSring));
+        System.out.println(andThen.apply(777));
+        
+        System.out.println("//        б) в порядке f3 -> f1 -> f2");
+        Function<String, String> compose = lengthOfSring.andThen(toString.compose(multiply));
+        System.out.println(compose.apply("Hello!"));
+    }
+}
