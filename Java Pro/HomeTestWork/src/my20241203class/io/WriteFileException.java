2f8fdc8d7dcc3c36493c218b65af05f767a62a18
Yevhen Kariev <mediams@mail.ru>
2024-12-03 23:14:28 +0100

    03-12-2024 23:14 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241203class/io/WriteFileException.java
+++ Java Pro/HomeTestWork/src/my20241203class/io/WriteFileException.java
@@ -3,45 +3,45 @@ package my20241203class.io;
 import java.io.*;
 
 public class WriteFileException {
-    public static void main(String[] args) throws IOException {
-        File file = new File("test.txt");
-        System.out.println(file.exists());
-        file.createNewFile();
-        System.out.println(file.exists());
-
+    public static void main(String[] args)  {
+//        File file = new File("test.txt");
+//        System.out.println(file.exists());
+//        file.createNewFile();
+//        System.out.println(file.exists());
+//
+////        try {
+////            Writer writer = new FileWriter(file);
+////            writer.write("1");
+////            writer.close();
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////            throw new RuntimeException(e);
+////        }
+//
+//        Writer writer = null;
+//        BufferedWriter bufferedWriter = null;
 //        try {
-//            Writer writer = new FileWriter(file);
-//            writer.write("1");
-//            writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
-//        }
-
-        Writer writer = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            writer = new FileWriter(file, true);
-            bufferedWriter = new BufferedWriter(writer);
-            bufferedWriter.newLine();
-            bufferedWriter.write("abc");
-            bufferedWriter.newLine();
-            bufferedWriter.write("123");
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (writer == null) {
-                    writer.close();
-                }
-                if (bufferedWriter == null) {
-
-                    bufferedWriter.close();
-                }
-            }
-        }
-
+//            writer = new FileWriter(file, true);
+//            bufferedWriter = new BufferedWriter(writer);
+//            bufferedWriter.newLine();
+//            bufferedWriter.write("abc");
+//            bufferedWriter.newLine();
+//            bufferedWriter.write("123");
+//
+//
+////        } catch (IOException e) {
+////            throw new RuntimeException(e);
+////        } finally {
+////            try {
+////                if (writer == null) {
+////                    writer.close();
+////                }
+////                if (bufferedWriter == null) {
+////
+////                    bufferedWriter.close();
+////                }
+////            }
+////        }
+//
     }
 }
