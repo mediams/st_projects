70f6da997b7f11d697d4667c6e475a4b6ed26f9f
Yevhen Kariev <mediams@mail.ru>
2024-12-05 10:29:09 +0100

    05-12-2024 10:29 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/my20241205class/ReadWriteBinaryDataJava.java
@@ -0,0 +1,31 @@
+package my20241205class;
+
+import java.io.*;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+public class ReadWriteBinaryDataJava {
+    public static void main(String[] args) {
+        byte data = 0b101;
+//        data = 5;
+
+        try (OutputStream stream = new FileOutputStream(".\\resources\\binaryFile")){
+            stream.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (InputStream inputStream = new FileInputStream(".\\resources\\binaryFile")){
+            int input = inputStream.read();
+            System.out.println(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (InputStream inputStream = new FileInputStream(".\\resources\\AdobeStock_55929654.jpeg")){
+            int input = inputStream.read();
+            System.out.println(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
