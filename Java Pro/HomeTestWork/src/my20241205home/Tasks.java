ba4c0178c57aa435091c292074d7c9d755f1b5e4
Yevhen Kariev <mediams@mail.ru>
2024-12-05 16:35:29 +0100

    05-12-2024 16:35 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241205home/Tasks.java
+++ Java Pro/HomeTestWork/src/my20241205home/Tasks.java
@@ -1,9 +1,17 @@
 package my20241205home;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Tasks {
     public static void main(String[] args) {
 //        Напишите программу, которая создает текстовый файл output.txt и записывает в него строку "Привет, мир!".
 //        Убедитесь, что файл создается в корневой папке вашего проекта.
 
+        try (FileWriter writer = new FileWriter(".\\resources\\output.txt")) {
+            writer.write("Привет, мир!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
