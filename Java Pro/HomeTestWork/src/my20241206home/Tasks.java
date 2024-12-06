071ae563b3ceb4be4a259b862fd53b272f763d46
Yevhen Kariev <mediams@mail.ru>
2024-12-06 16:00:19 +0100

    06-12-2024 16:00 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/my20241206home/Tasks.java
+++ Java Pro/HomeTestWork/src/my20241206home/Tasks.java
@@ -1,19 +1,55 @@
 package my20241206home;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Tasks {
     public static void main(String[] args) {
 //        1. Чтение текста из файла и вывод на экран
 //        Напишите программу, которая читает текстовый файл input.txt и выводит его содержимое на экран.
 //        Используйте блок try-catch для обработки возможных ошибок, таких как отсутствие файла.
-        try (FileWriter writer = new FileWriter(".\\resources\\input.txt");){
-            writer.write("Напишите программу, которая читает текстовый файл input.txt и выводит его содержимое на экран.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        Path path = Paths.get("resources", "input.txt");
+        String text = "Напишите программу, которая читает текстовый файл input.txt и выводит его содержимое на экран.";
+        createFileWithText(path, text);
+
+        readFile(path);
+//        Запись текста в файл
+//        Напишите программу, которая запрашивает у пользователя текст через консоль и записывает его в файл output.txt.
+//        Если файл уже существует, он должен быть перезаписан. Добавьте обработку исключений для случаев, если что-то пойдёт не так.
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter some text: ");
+            String string = sc.nextLine();
+            if (string.isEmpty()) {
+                System.out.println("String is Empty!");
+                return;
+            }
+            createFileWithText(path, string);
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода-вывода: " + e.getMessage());
         }
+    }
 
+    private static void createFileWithText(Path path, String text) {
+        try (FileWriter writer = new FileWriter(String.valueOf(path));) {
+            writer.write(text);
+        } catch (IOException e) {
+            System.err.println("Error!" + e.getMessage());
+        }
+    }
 
+    private static void readFile(Path path) {
+        try (BufferedReader br = new BufferedReader(new FileReader(String.valueOf(path)));) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found!" + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Read error!" + e.getMessage());
+        }
     }
 }
