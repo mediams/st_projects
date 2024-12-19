8b23de383296acdf87fc200ee7e553ac3ea71dc9
Yevhen Kariev <mediams@mail.ru>
2024-12-19 14:58:58 +0100

    19-12-2024 14:58 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/main/java/my20241219practise/Sync1.java
@@ -0,0 +1,14 @@
+package my20241219practise;
+
+public class Sync1 {
+//    int a;
+//
+//    synchronized (Sync1.class){
+//        int b;
+//    }
+    public static void main(String[] args) {
+
+
+
+    }
+}
