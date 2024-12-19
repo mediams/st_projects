ec8dbf9576318480a5dc7484239b973d4aa641bc
Yevhen Kariev <mediams@mail.ru>
2024-12-19 22:28:01 +0100

    19-12-2024 22:27 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/main/java/my20241219home/Main.java
@@ -0,0 +1,16 @@
+package my20241219home;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread taskOne = new Thread(new MyThreadOne());
+        taskOne.start();
+
+        Thread taskTwo = new Thread(new MyThreadTwo());
+        taskTwo.start();
+
+        List<Integer> integerList = new ArrayList<>(taskOne.start());
+    }
+}
---
+++ Java Pro/HomeTestWork/src/main/java/my20241219home/MyThreadTwo.java
@@ -0,0 +1,19 @@
+package my20241219home;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyThreadTwo implements Runnable{
+    List<Integer> integerList = new ArrayList<>();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                integerList.add(i);
+                System.out.println("Thread-1: " + i);
+            }
+        }
+        System.out.println(integerList);
+    }
+}
