e968bf95128fb1c647f200f392e801699e342798
Yevhen Kariev <mediams@mail.ru>
2025-01-07 22:10:44 +0100

    07-01-2025 22:10 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250107home/LockEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20250107home/LockEx.java
@@ -1,15 +1,40 @@
 package my20250107home;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockEx {
-    private final Lock lock = new ReentrantLock();
+    private static final Lock lock = new ReentrantLock();
 
     public static void main(String[] args) {
         LockEx lockEx = new LockEx();
         lockEx.performTask();
 
+        Runnable task = () -> {
+            try {
+                if (lock.tryLock(1, TimeUnit.SECONDS)) {
+
+                    try {
+                        System.out.println(Thread.currentThread().getName() + ", get thread");
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        lock.unlock();
+                        System.out.println(Thread.currentThread().getName()+ "Left the thread");
+                    }
+                } else System.out.println(Thread.currentThread().getName() + ", can't take the thread");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread t1 = new Thread(task, "Thread One");
+        Thread t2 = new Thread(task, "Thread Two");
+
+        t1.start();
+        t2.start();
     }
 
     public void performTask() {
@@ -19,7 +44,7 @@ public class LockEx {
         } finally {
             lock.unlock();
         }
-
-
     }
+
+
 }
