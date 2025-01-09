ca0e3325e244c2d93421948db3b053bc01a62e12
Yevhen Kariev <mediams@mail.ru>
2025-01-09 18:58:28 +0100

    09-01-2025 18:58 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/main/java/my20250109home/AccountPlus10.java
@@ -0,0 +1,31 @@
+package my20250109home;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AccountPlus10 {
+    public static void main(String[] args) {
+        AtomicInteger count = new AtomicInteger(0);
+
+        Thread t1 = new Thread(() -> {
+            while (count.get()<=100) {
+                count.addAndGet(10);
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            while (count.get()<=100) {
+                count.addAndGet(10);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(count.get());
+    }
+}
---
+++ Java Pro/HomeTestWork/src/main/java/my20250109home/AtomicEx.java
@@ -0,0 +1,25 @@
+package my20250109home;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicEx {
+    public static void main(String[] args) {
+        AtomicInteger count = new AtomicInteger(0);
+
+
+        new Thread(() -> {
+            for (int i = 0; i <100; i++) {
+                count.incrementAndGet();
+            }
+        }).start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(count.get());
+    }
+}
