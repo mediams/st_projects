21e3d7dacf3ef80198de2f3c89c5386a713854be
Yevhen Kariev <mediams@mail.ru>
2025-01-09 20:18:48 +0100

    09-01-2025 20:18 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250108homework/BlockingQueueEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20250108homework/BlockingQueueEx.java
@@ -1,6 +1,53 @@
 package my20250108homework;
 
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public class BlockingQueueEx {
+    static BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) {
+        var producer = new Producer();
+
+        new Thread(producer::generate).start();
+        new Thread(() -> new Consumer().take()).start();
+
+    }
+
+    static class Producer {
+        List<String> stringList = List.of("Text_1", "Text_2", "Text_3", "Text_4", "Text_5", "exit");
+
+        public void generate() {
+            for (String s : stringList) {
+                try {
+                    queue.put(s);
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class Consumer {
+        public void take() {
+            while (true) {
+                try {
+                    String temp = queue.take();
+                    if (temp.equals("exit")) {
+                        System.out.println("EXIT.");
+                        return;
+                    } else {
+                        System.out.println(temp);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
 //2. Применить BlockingQueue:
 //Класс Продьюсер - генерирует сообщения в очередь
