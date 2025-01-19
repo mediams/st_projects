4c98ddc581888357405d11d50106342bf583e8b9
Yevhen Kariev <mediams@mail.ru>
2025-01-19 20:17:17 +0100

    19-01-2025 20:17 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250118homework/ReflectionEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20250118homework/ReflectionEx.java
@@ -3,20 +3,38 @@ package my20250118homework;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public class ReflectionEx {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         Class<MyStack> myStackClass = MyStack.class;
-        Method[] declaredMethods = myStackClass.getDeclaredMethods();
-        Arrays.stream(declaredMethods).forEach(method -> {
-            System.out.println(method.getName() + ", private: " + Modifier.isPrivate(Modifier.fieldModifiers()));
-        });
-        MyStack myStack = myStackClass.getConstructor(int.class).newInstance(1);
+
+        MyStack myStack = myStackClass.getConstructor(int.class).newInstance(5);
         Method methodPush = myStackClass.getDeclaredMethod("push", String.class);
         methodPush.setAccessible(true);
         methodPush.invoke(myStack, "Test string");
+        System.out.println("Object after push(): " + myStack);
+        Method methodPop = myStackClass.getDeclaredMethod("pop");
+        methodPop.setAccessible(true);
+        Object popped = methodPop.invoke(myStack);
+        System.out.println("Object after push(): " + myStack);
+        System.out.println("Popped value: " + popped);
+
+        System.out.println("===========================");
+        myStack.push("A");
+        myStack.push("B");
+        myStack.push("C");
+        myStack.push("D");
+        myStack.push("E");
+
+        Field data = myStackClass.getDeclaredField("data");
+        data.setAccessible(true);
+
+        String[] oldData = (String[]) data.get(myStack);
+        String[] newData = Arrays.copyOf(oldData, oldData.length + 1);
+        data.set(myStack, newData);
+
+        methodPush.invoke(myStack, "F");
         System.out.println(myStack);
 
     }
