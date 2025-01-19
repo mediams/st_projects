718691e7d43c87754789af83857bb8cf40530df2
Yevhen Kariev <mediams@mail.ru>
2025-01-19 17:22:57 +0100

    19-01-2025 17:22 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250118homework/ReflectionEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20250118homework/ReflectionEx.java
@@ -1,7 +1,23 @@
 package my20250118homework;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 public class ReflectionEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<MyStack> myStackClass = MyStack.class;
+        Method[] declaredMethods = myStackClass.getDeclaredMethods();
+        Arrays.stream(declaredMethods).forEach(method -> {
+            System.out.println(method.getName() + ", private: " + Modifier.isPrivate(Modifier.fieldModifiers()));
+        });
+        MyStack myStack = myStackClass.getConstructor(int.class).newInstance(1);
+        Method methodPush = myStackClass.getDeclaredMethod("push", String.class);
+        methodPush.setAccessible(true);
+        methodPush.invoke(myStack, "Test string");
+        System.out.println(myStack);
 
     }
 }
