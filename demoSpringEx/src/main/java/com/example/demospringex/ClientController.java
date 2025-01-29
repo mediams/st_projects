5fc82ab6565a40cca49c0a36827c0e354fbd76ed
Yevhen Kariev <yevhenkariev@gmail.com>
2025-01-29 16:42:28 +0100

    29-01-2025 16:42 from PC 'Yevhen Kariev'

--- demoSpringEx/src/main/java/com/example/demospringex/ClientController.java
+++ demoSpringEx/src/main/java/com/example/demospringex/ClientController.java
@@ -1,5 +1,7 @@
 package com.example.demospringex;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
