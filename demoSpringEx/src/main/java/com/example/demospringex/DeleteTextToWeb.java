e1d5fdc5964cfbd17b2b905e523bbccf25d0af70
Yevhen Kariev <yevhenkariev@gmail.com>
2025-01-29 15:15:09 +0100

    29-01-2025 15:15 from PC 'Yevhen Kariev'

--- demoSpringEx/src/main/java/com/example/demospringex/DeleteTextToWeb.java
+++ demoSpringEx/src/main/java/com/example/demospringex/DeleteTextToWeb.java
@@ -1,16 +1,24 @@
 package com.example.demospringex;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DeleteTextToWeb {
     @GetMapping("/delete")
     @ResponseBody
     public String deleteTextToWeb() {
         return "textToWeb";
+    }
+
+    @GetMapping("/greet")
+    public String deleteTextToWeb2(@RequestParam(name = "name", required = false, defaultValue = "Gast") String name, Model model) {
+        model.addAttribute("message", "Hello " + name  );
+        return "textToWeb2";
     }
 
 }
