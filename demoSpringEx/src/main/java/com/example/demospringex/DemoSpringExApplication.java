e3d06229518b1b29bea7b8c2c97b793af445241e
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-01 14:31:53 +0100

    01-02-2025 14:31 from PC 'Yevhen Kariev'

--- demoSpringEx/src/main/java/com/example/demospringex/DemoSpringExApplication.java
+++ demoSpringEx/src/main/java/com/example/demospringex/DemoSpringExApplication.java
@@ -2,12 +2,13 @@ package com.example.demospringex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoSpringExApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringExApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(DemoSpringExApplication.class, args);
     }
 
 }
