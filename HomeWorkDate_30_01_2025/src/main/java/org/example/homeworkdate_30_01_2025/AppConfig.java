850c422662c4bc7fc8e161cdd70377ff341e5d72
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-02 21:22:37 +0100

    02-02-2025 21:22 from PC 'Yevhen Kariev'

--- HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/AppConfig.java
+++ HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/AppConfig.java
@@ -1,7 +1,18 @@
 package org.example.homeworkdate_30_01_2025;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
 
 @Configuration
 public class AppConfig {
+
+    @Bean("visa")
+    public Order order() {
+        Order order = new Order();
+        order.setItem("VISA");
+        order.setPrice(BigDecimal.valueOf(2.0));
+        return order;
+    }
 }
--- HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/HomeWorkDate30012025Application.java
+++ HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/HomeWorkDate30012025Application.java
@@ -14,16 +14,16 @@ public class HomeWorkDate30012025Application {
 //        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Order visa = (Order)context.getBean("visa");
+        Order visa = context.getBean("visa", Order.class);
         System.out.println(visa);
-        Order paypal = (Order)context.getBean("paypal");
-        System.out.println(paypal);
+//        Order paypal = (Order)context.getBean(Order.class);
+//        System.out.println(paypal);
 
-        PaymentGateway order = context.getBean("orderPayPal", PaymentGateway.class);
-        System.out.println(order);
-
-        PaymentGateway orderVisa = context.getBean("orderVisa", PaymentGateway.class);
-        System.out.println(orderVisa);
+//        PaymentGateway order = context.getBean("orderPayPal", PaymentGateway.class);
+//        System.out.println(order);
+//
+//        PaymentGateway orderVisa = context.getBean("orderVisa", PaymentGateway.class);
+//        System.out.println(orderVisa);
 
 
     }
