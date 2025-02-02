09b82ea85ea89eba1567784339d8a165d71633b1
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-02 17:47:37 +0100

    02-02-2025 17:47 from PC 'Yevhen Kariev'

---
+++ HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/AppConfig.java
@@ -0,0 +1,7 @@
+package org.example.homeworkdate_30_01_2025;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
--- HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/HomeWorkDate30012025Application.java
+++ HomeWorkDate_30_01_2025/src/main/java/org/example/homeworkdate_30_01_2025/HomeWorkDate30012025Application.java
@@ -3,6 +3,7 @@ package org.example.homeworkdate_30_01_2025;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
@@ -10,16 +11,18 @@ public class HomeWorkDate30012025Application {
 
     public static void main(String[] args) {
 //        SpringApplication.run(HomeWorkDate30012025Application.class, args);
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
         Order visa = (Order)context.getBean("visa");
         System.out.println(visa);
         Order paypal = (Order)context.getBean("paypal");
         System.out.println(paypal);
-        
-        PaymentGateway order = (PaymentGateway)context.getBean("orderPayPal");
+
+        PaymentGateway order = context.getBean("orderPayPal", PaymentGateway.class);
         System.out.println(order);
 
-        PaymentGateway orderVisa = (PaymentGateway)context.getBean("orderVisa");
+        PaymentGateway orderVisa = context.getBean("orderVisa", PaymentGateway.class);
         System.out.println(orderVisa);
 
 
