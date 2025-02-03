84f135deeda24b924fcca0225f82ddb371930158
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-03 11:00:04 +0100

    03-02-2025 11:00 from PC 'Yevhen Kariev'

---
+++ Java Pro/HomeTestWork/src/main/java/search/LinearSearchEx.java
@@ -0,0 +1,21 @@
+package search;
+
+public class LinearSearchEx {
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 5;
+
+        System.out.println(linearSearch(array, key));
+
+    }
+
+}
