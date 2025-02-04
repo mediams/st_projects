4039cd7b3ece2733b98c6280d3493d0687b91683
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-04 11:51:00 +0100

    04-02-2025 11:50 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250203home/search/Desktop.java
+++ Java Pro/HomeTestWork/src/main/java/my20250203home/search/Desktop.java
@@ -3,7 +3,6 @@ package my20250203home.search;
 
 public class Desktop implements Computer{
     @Override
-    @Qua
     public void compile() {
         System.out.println("Compile Desktop");
 
---
+++ Java Pro/HomeTestWork/src/main/java/my20250204home/ArraysEx.java
@@ -0,0 +1,23 @@
+package my20250204home;
+
+public class ArraysEx {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3}, {4, 5, 6, 7, 8}, {9, 10}};
+
+        for (int[] b : a) {
+            for (int c : b) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 5; j++) {
+                if (i == 0 || i ==3 || j == 0 || j ==5) {
+                    System.out.print("* ");
+                } else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
