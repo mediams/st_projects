3187f1fd61a48c6cb4a397c128276e37d9cf57a5
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-06 16:07:57 +0100

    06-02-2025 16:07 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250206home/DemoJdbc.java
+++ Java Pro/HomeTestWork/src/main/java/my20250206home/DemoJdbc.java
@@ -1,27 +1,45 @@
 package my20250206home;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DemoJdbc {
     public static void main(String[] args) throws SQLException {
 
         /*
-        * import package
-        * load and register
-        * create connection
-        * create statement
-        * execute statement
-        * process the result
-        * close
-        * */
+         * import package
+         * load and register
+         * create connection
+         * create statement
+         * execute statement
+         * process the result
+         * close
+         * */
 
-        String url = "jdbc:mysql://localhost:3306/demo";
+        String url = "jdbc:mysql://localhost:3306/world";
         String user = "root";
         String password = "338778";
 
-        Connection conn = DriverManager.getConnection("","","");
+        Connection conn = DriverManager.getConnection(url, user, password);
+        Statement stmt = conn.createStatement();
+        String sql = "select * from city where id = 2";
+        ResultSet rs = stmt.executeQuery(sql);
 
+//        rs.next(); //ID, Name, CountryCode, District, Population
+//        System.out.println(rs.getInt("ID"));
+//        System.out.println(rs.getString("Name"));
+//        System.out.println(rs.getString("CountryCode"));
+//        System.out.println(rs.getString("District"));
+//        System.out.println(rs.getString("Population"));
+//        System.out.println("Connected to database: " + rs);
+
+        while (rs.next()) {
+            System.out.print(rs.getInt(1) + " - ");
+            System.out.print(rs.getString(2) + " - ");
+            System.out.print(rs.getString(3)+ " - ");
+            System.out.print(rs.getString(4)+ " - ");
+            System.out.println(rs.getString(5));
+        }
+
+        rs.close();
     }
 }
