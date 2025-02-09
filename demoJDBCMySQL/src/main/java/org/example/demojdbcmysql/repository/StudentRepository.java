840e3af25d235063adc29903f33375cc0451a20a
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-09 14:37:18 +0100

    09-02-2025 14:37 from PC 'Yevhen Kariev'

--- demoJDBCMySQL/src/main/java/org/example/demojdbcmysql/repository/StudentRepository.java
+++ demoJDBCMySQL/src/main/java/org/example/demojdbcmysql/repository/StudentRepository.java
@@ -4,14 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.example.demojdbcmysql.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
