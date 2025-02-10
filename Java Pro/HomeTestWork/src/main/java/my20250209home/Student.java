0632aaa318c8fa3bb9e328624de9c6763d651185
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-10 08:56:50 +0100

    10-02-2025 08:56 from PC 'Yevhen Kariev'

--- Java Pro/HomeTestWork/src/main/java/my20250209home/Student.java
+++ Java Pro/HomeTestWork/src/main/java/my20250209home/Student.java
@@ -1,4 +1,16 @@
 package my20250209home;
 
 public class Student {
+    private int id = 1;
+    private String name = "Student";
+    private int age = 10;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- Java Pro/HomeTestWork/src/main/java/my20250209home/TasksEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20250209home/TasksEx.java
@@ -1,4 +1,18 @@
 package my20250209home;
 
 public class TasksEx {
+    public static void main(String[] args) {
+        Object [] obj = new Object [10];
+        obj[0] = 1;
+        obj[1] = "Hello";
+        obj[2] = "World";
+        obj[3] = false;
+        obj[4] = true;
+        obj[5] = new Student();
+
+        for (Object o : obj) {
+
+            System.out.println(o);
+        }
+    }
 }
