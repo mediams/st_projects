03aff2f22426104d07529e28edf037ce868b6b77
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-14 21:34:02 +0100

    14-02-2025 Committed updates - from PC

--- Java Pro/HomeTestWork/src/main/java/my20251402home/CollectionsEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20251402home/CollectionsEx.java
@@ -6,6 +6,14 @@ import java.util.*;
 
 public class CollectionsEx {
     public static void main(String[] args) {
+
+        Comparator<Integer> comparator = new Comparator<>() {
+            @Override
+            public int compare(Integer o2, Integer o1) {
+                return o1 - o2;
+            }
+        };
+
         int[] intArray  = {1, 2};;
         intArray[0] = 13;
         intArray[1] = 21;
@@ -22,6 +30,12 @@ public class CollectionsEx {
 
         integerList.add(1);
         integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.add(5);
+
+        Collections.sort(integerList, comparator);
+        System.out.println(integerList);
 
         System.out.println(integerList.get(1));
         System.out.println(integerList.indexOf(1));
@@ -29,5 +43,17 @@ public class CollectionsEx {
         Iterator<Integer> iterator = integerList.iterator();
         while (iterator.hasNext())
             System.out.println(iterator.next() + " - ");
+
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Test-1");
+        map.put(2, "Test-2");
+        map.put(3, "Test-3");
+
+        Set<Integer> integers = map.keySet();
+        for (Integer integer : integers) {
+            System.out.println(integer + " - " + map.get(integer));
+        }
+
     }
 }
