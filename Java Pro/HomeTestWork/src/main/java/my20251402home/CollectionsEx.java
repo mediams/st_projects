67dd6499c5bae912d7f7472b5b28f16b6b700653
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-14 18:41:39 +0100

    14-02-2025 Minor tweaks - from PC

---
+++ Java Pro/HomeTestWork/src/main/java/my20251402home/CollectionsEx.java
@@ -0,0 +1,11 @@
+package my20251402home;
+
+public class CollectionsEx {
+    public static void main(String[] args) {
+        int[] intArray  = {1, 2};;
+        intArray[0] = 13;
+        intArray[1] = 21;
+
+        System.out.println(intArray[1]);
+    }
+}
