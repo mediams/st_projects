d09afea5bd90526bbe72830e67d1491ad6a27e54
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-16 21:36:02 +0100

    16-02-2025 Committed updates - from PC

--- Java Pro/HomeTestWork/src/main/java/my20250216home/TaskEx.java
+++ Java Pro/HomeTestWork/src/main/java/my20250216home/TaskEx.java
@@ -1,6 +1,7 @@
 package my20250216home;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name, int distance) {
     @Override
@@ -13,12 +14,47 @@ public class TaskEx {
     public static void main(String[] args) {
         LinkedList<Place> placeToVisit = new LinkedList<>();
 
-        Place adelaide = new Place("Adelaide", 1374);
-        Place adelaide2 = new Place("adelaide", 1374);
-        addPlace(placeToVisit, adelaide);
-        addPlace(placeToVisit, adelaide2);
+        addPlace(placeToVisit, new Place("Adelaide", 1374));
+        addPlace(placeToVisit, new Place("adelaide", 1374));
+        addPlace(placeToVisit, new Place("Alice", 917));
+        addPlace(placeToVisit, new Place("Springs", 3923));
+        addPlace(placeToVisit, new Place("Brisbane", 2771));
+        addPlace(placeToVisit, new Place("Darwin", 3972));
+        addPlace(placeToVisit, new Place("Melbourne", 877));
+        addPlace(placeToVisit, new Place("Perth", 374));
+
+        placeToVisit.addFirst(new Place("Sydney", 0));
+
         System.out.println(placeToVisit);
 
+        var iterator = placeToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+        printMessage();
+
+        while (!quitLoop) {
+            System.out.println("Enter value: ");
+            String menuItem = scanner.nextLine().toLowerCase().substring(0, 1);
+
+            switch (menuItem) {
+                case "f" -> {
+                    System.out.println("User want to go forward");
+                }
+                case "b" -> {
+                    System.out.println("User want to go backrwards");
+                }
+                case "m" -> {
+                    printMessage();
+                }
+                case "l" -> {
+                    System.out.println(placeToVisit);
+                }
+                default -> {
+                    quitLoop = true;
+                }
+            }
+        }
     }
 
     private static void addPlace(LinkedList<Place> list, Place place) {
@@ -35,6 +71,15 @@ public class TaskEx {
 
         }
 //        else System.out.println("added : " + place);
+
+        int matchIndex = 0;
+        for (var distance : list) {
+            if (place.distance() < distance.distance()) {
+                list.add(matchIndex, place);
+                return;
+            }
+            matchIndex++;
+        }
         list.add(place);
     }
 
