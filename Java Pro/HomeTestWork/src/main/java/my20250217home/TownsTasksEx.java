40445adae6b64400b3f8110a30431d424a02ec66
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-17 22:13:33 +0100

    17-02-2025 Saved changes - from PC

---
+++ Java Pro/HomeTestWork/src/main/java/my20250217home/TownsTasksEx.java
@@ -0,0 +1,38 @@
+package my20250217home;
+
+import java.util.LinkedList;
+
+record Town(String name, int distance) {
+    @Override
+    public String toString() {
+        return String.format("\n%s (%d)", name, distance);
+    }
+
+}
+
+public class TownsTasksEx {
+    private static LinkedList<Town> towns = new LinkedList<>();
+
+    public static void main(String[] args) {
+        addPlace(new Town("Kiyv", 800));
+        addPlace(new Town("Avdiivka", 0));
+        addPlace(new Town("Yasinuvata", 12));
+        addPlace(new Town("Donetsk", 15));
+        addPlace(new Town("Lviv", 1200));
+        addPlace(new Town("Vinnica", 600));
+
+        System.out.println(towns);
+    }
+
+    private static void addPlace(Town town) {
+        int indexMatcher = 0;
+        for (Town city : towns) {
+            if (city.distance() > town.distance()) {
+                towns.add(indexMatcher, town);
+                return;
+            }
+            indexMatcher++;
+        }
+        towns.add(town);
+    }
+}
