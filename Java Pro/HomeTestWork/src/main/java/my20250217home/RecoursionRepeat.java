915e3f559d4f3e70e6250e6d61ee2ac8bb5165c6
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-23 23:08:32 +0100

    23-02-2025 Committed updates - from PC

--- Java Pro/HomeTestWork/src/main/java/my20250217home/RecoursionRepeat.java
+++ Java Pro/HomeTestWork/src/main/java/my20250217home/RecoursionRepeat.java
@@ -6,6 +6,30 @@ public class RecoursionRepeat {
         System.out.println(miltiOfNNumber(5));
         reverseNummer(123);
         System.out.println("\n" + linearSearch(new int[]{1, 2, 3, 4, 5}, 4));
+        reverseWhile(123);
+
+        System.out.println("--------------");
+        int[] array = {1, 3, 4, 5, 6, 7, 8, 9, 10, 18};
+        int key = 18;
+
+        System.out.printf("in Position: %d", binarySearch(array, key));
+    }
+
+    private static int binarySearch(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mit = left + (right - left) / 2;
+//            int mit = (left + right)  / 2;
+            if (array[mit] == key) {
+                return mit;
+            } else if (array[mit] > key) {
+                right = mit - 1;
+            }else
+                left = mit + 1;
+        }
+        return -1;
     }
 
     private static int sumOfNNumber(int n) {
@@ -38,5 +62,12 @@ public class RecoursionRepeat {
             }
         }
         return -1;
+    }
+
+    private static void reverseWhile(int n) {
+        while (n != 0) {
+            System.out.print(n % 10);
+            n /= 10;
+        }
     }
 }
