23973de1e6beb41ec3a0cde4653198c2acbb547b
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-25 14:46:57 +0100

    25-02-2025 Auto-saved changes - from PC

---
+++ Java Pro/HomeTestWork/src/main/java/my20250225home/ConverterToHoursAndMinutes.java
@@ -0,0 +1,24 @@
+package my20250225home;
+
+public class ConverterToHoursAndMinutes {
+    public static void main(String[] args) {
+        System.out.println(convertToHoursAndMinutes(3711));
+    }
+
+    private static String convertToHoursAndMinutes(int seconds) {
+        int minutes = seconds / 60;
+        int hours = minutes / 60;
+        int restMinutes = minutes % 60;
+        int restSeconds = seconds % 60;
+
+        System.out.println(minutes + ": minutes = seconds / 60,\n"
+                + hours + ": hours = minutes / 60,\n"
+                + restMinutes + ": restMinutes = minutes % 60,\n"
+                + restSeconds + ": restSeconds = seconds % 60," );
+        return "" + hours + " " + restMinutes + " " + restSeconds;
+    }
+
+    private static void convertToHoursAndMinutes(int minutes, int seconds) {
+
+    }
+}
