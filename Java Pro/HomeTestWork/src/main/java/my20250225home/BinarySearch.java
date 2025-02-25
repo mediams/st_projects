e6d8f40a7a224f58fae665e02f6bd6aededb9e08
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-25 15:28:50 +0100

    25-02-2025 fixed - from PC

--- Java Pro/HomeTestWork/src/main/java/my20250225home/BinarySearch.java
+++ Java Pro/HomeTestWork/src/main/java/my20250225home/BinarySearch.java
@@ -5,6 +5,7 @@ public class BinarySearch {
         int[] array = {1, 2, 3, 5, 6, 7, 4, 8, 9};
         int key = 9;
         System.out.println(binarySearch(array, key));
+        System.out.println(binarySearch(key, array, 0, array.length - 1));
     }
 
     private static int binarySearch(int[] array, int key) {
@@ -21,5 +22,19 @@ public class BinarySearch {
             } else left = mid + 1;
         }
         return -1;
+    }
+
+    private static int binarySearch(int key, int[] array, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+
+        if (array[mid] == key) {
+            return mid;
+        } else if (array[mid] > key) {
+            return binarySearch(key, array, left, mid - 1);
+//                right = mid - 1;
+        } else return binarySearch(key, array, mid + 1, right);//left = mid + 1;
     }
 }
