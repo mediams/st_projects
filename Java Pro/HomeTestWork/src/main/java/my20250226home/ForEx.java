53f19e050c84b13a6d1b02155d1118a97d586c2e
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-26 20:37:40 +0100

    26-02-2025 Logic improved - from PC

---
+++ Java Pro/HomeTestWork/src/main/java/my20250226home/ForEx.java
@@ -0,0 +1,20 @@
+package my20250226home;
+
+public class ForEx {
+    public static void main(String[] args) {
+        for (int i = 0, j = 1; i < 5; i++, j--) {
+
+            System.out.println("Bu");
+        }
+
+        for (double i = 7.5; i <= 10; i += 0.25) {
+            double interestAmount = calculateInterest(100.0, i);
+            System.out.println("100 at " + i + "% intrest = " + interestAmount);
+        }
+
+    }
+
+    private static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}
