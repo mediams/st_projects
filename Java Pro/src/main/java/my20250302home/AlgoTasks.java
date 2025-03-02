777062a86760a7dad0f3d298ad2814d0764368d9
Yevhen Kariev <yevhenkariev@gmail.com>
2025-03-02 22:49:19 +0100

    02-03-2025 Finalized commit - from PC

--- Java Pro/src/main/java/my20250302home/AlgoTasks.java
+++ Java Pro/src/main/java/my20250302home/AlgoTasks.java
@@ -4,9 +4,26 @@ import java.util.Arrays;
 
 public class AlgoTasks {
     public static void main(String[] args) {
-        int[] array = {3, 4, 5, 2, 7, 1};
+        int[] array = {1, 2, 3, 4, 5, 7};
+        int key = 10;
 
         System.out.println(Arrays.toString(selectedSort(array)));
+        System.out.println(binarySearch(array, key));
+    }
+
+    private static int binarySearch(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == key) {
+                return mid;
+            } else if (array[mid] > key) {
+                right = mid - 1;
+            } else left = mid + 1;
+        }
+        return -1;
     }
 
     private static int[] selectedSort(int[] array) {
