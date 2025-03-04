352b942d0f173ed62d7d76b63a1586f261a1b479
Yevhen Kariev <yevhenkariev@gmail.com>
2025-03-04 14:51:36 +0100

    04-03-2025 Added meta information - from PC

--- Java Pro/src/main/java/my20250304home/EvenDigitSum.java
+++ Java Pro/src/main/java/my20250304home/EvenDigitSum.java
@@ -1,4 +1,27 @@
 package my20250304home;
 
 public class EvenDigitSum {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789)); //→ should return 20 since 2 + 4 + 6 + 8 = 20
+
+        System.out.println(getEvenDigitSum(252)); //→ should return 4 since 2 + 2 = 4
+
+        System.out.println(getEvenDigitSum(-22)); //→ should return -1 since the number is negative
+    }
+
+    public static int getEvenDigitSum(int i) {
+        if (i < 0) {
+            return -1;
+        }
+        int sum = 0;
+        while (i > 0) {
+            int number = i % 10;
+            if (number % 2 == 0) {
+                sum += number;
+            }
+            i /= 10;
+        }
+
+        return sum;
+    }
 }
